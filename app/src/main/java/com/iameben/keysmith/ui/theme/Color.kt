84f59f0bf22039f465decde7d4800b35eb1f495d
iAmEben <justiceebenezer4@gmail.com>
2025-06-14 22:11:20 +0100

    Updated app colour and theme

--- app/src/main/java/com/iameben/keysmith/ui/theme/Color.kt
+++ app/src/main/java/com/iameben/keysmith/ui/theme/Color.kt
@@ -9,3 +9,28 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Light mode
+val Beige = Color(0xFFFFEEDC)
+val DarkBrown = Color(0xFF4A2C1D)
+val MediumBrown = Color(0xFF6E4B3A)
+val GoldenBrown = Color(0xFFD8A157)
+val DeepBrown = Color(0xFF52341C)
+val WarmBrown = Color(0xFFC76C3D)
+val DarkChocolateBrown = Color(0xFF4A2C1D)
+val Red = Color(0xFFBF3F3F)
+val Orange = Color(0xFFE4823E)
+val YellowBrown = Color(0xFFCC9A3B)
+
+
+// Dark Mode
+val DeepCharcoalBlack = Color(0xFF0D0D0D)
+val WarmBeige = Color(0xFFD9A565)
+val MutedCocoa = Color(0xFF7B4E36)
+val GoldenOrange = Color(0xFFE99647)
+val SoftAmber = Color(0xFFB97B51)
+val BurntOrange = Color(0xFFC05926)
+val DarkMocha = Color(0xFF3B2312)
+val DeepRed = Color(0xFFA5442C)
+val RusticOrange = Color(0xFFC96B2D)
+val Gold = Color(0xFFD5A045)
