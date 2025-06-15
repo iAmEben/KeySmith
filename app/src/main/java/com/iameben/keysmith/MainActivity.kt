76d82de7970629c9fc40d982465dabf6b2dde33d
iAmEben <justiceebenezer4@gmail.com>
2025-06-15 01:36:05 +0100

    Updated colours for light and dark theme + added function to prevent white screen before splashscreen

--- app/src/main/java/com/iameben/keysmith/MainActivity.kt
+++ app/src/main/java/com/iameben/keysmith/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.iameben.keysmith.navigation.NavGraph
 import com.iameben.keysmith.ui.theme.KeySmithTheme
@@ -16,11 +17,12 @@ class MainActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
             KeySmithTheme {
+                val navController = rememberNavController()
                 NavGraph(
                     navController = navController
                 )
--- app/src/main/java/com/iameben/keysmith/ui/theme/Color.kt
+++ app/src/main/java/com/iameben/keysmith/ui/theme/Color.kt
@@ -2,13 +2,6 @@ package com.iameben.keysmith.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
 
 // Light mode
 val Beige = Color(0xFFFFEEDC)
@@ -17,7 +10,9 @@ val MediumBrown = Color(0xFF6E4B3A)
 val GoldenBrown = Color(0xFFD8A157)
 val DeepBrown = Color(0xFF52341C)
 val WarmBrown = Color(0xFFC76C3D)
-val DarkChocolateBrown = Color(0xFF4A2C1D)
+val DeepCocoaBrown = Color(0xFF4A2C1D)
+val PureWhite = Color(0xFFFFFFFF)
+val LightApricotBeige = Color(0xFFFFF3E5)
 val Red = Color(0xFFBF3F3F)
 val Orange = Color(0xFFE4823E)
 val YellowBrown = Color(0xFFCC9A3B)
@@ -25,8 +20,10 @@ val YellowBrown = Color(0xFFCC9A3B)
 
 // Dark Mode
 val DeepCharcoalBlack = Color(0xFF0D0D0D)
+val WarmSandTan = Color(0xFFD9A56F)
 val WarmBeige = Color(0xFFD9A565)
 val MutedCocoa = Color(0xFF7B4E36)
+val DeepAshGray = Color(0xFF1C1C1C)
 val GoldenOrange = Color(0xFFE99647)
 val SoftAmber = Color(0xFFB97B51)
 val BurntOrange = Color(0xFFC05926)
