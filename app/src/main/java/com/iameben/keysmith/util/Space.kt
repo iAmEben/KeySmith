bb9194fc39588422030ba19591169281f864fb06
iAmEben <justiceebenezer4@gmail.com>
2025-06-17 22:15:59 +0100

    Spacer Util class to avoid boilerplate codes

---
+++ app/src/main/java/com/iameben/keysmith/util/Space.kt
@@ -0,0 +1,24 @@
+package com.iameben.keysmith.util
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Space(
+    size: Dp = 8.dp,
+    height: Dp = 0.dp,
+    width: Dp = 0.dp
+) {
+    Spacer(
+        modifier = Modifier
+            .size(size)
+            .height(height)
+            .width(width)
+    )
+}
