0faecbf48e1aeb00d621d1303735f042c7b98e0d
iAmEben <justiceebenezer4@gmail.com>
2025-06-19 22:51:30 +0100

    Added init function to get users app theme and set app accordingly

--- app/src/main/java/com/iameben/keysmith/ui/screen/main/ThemeViewmodel.kt
+++ app/src/main/java/com/iameben/keysmith/ui/screen/main/ThemeViewmodel.kt
@@ -4,13 +4,18 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class ThemeViewmodel @Inject constructor() : ViewModel() {
 
     private val _isDarkTheme = MutableStateFlow(false)
-    val isDarkTheme = _isDarkTheme.asStateFlow()
+    val isDarkTheme: StateFlow<Boolean> = _isDarkTheme.asStateFlow()
+
+    fun initializeTheme(isSystemInDarkTheme: Boolean) {
+        _isDarkTheme.value = isSystemInDarkTheme
+    }
 
     fun toggleTheme() {
         _isDarkTheme.value = !_isDarkTheme.value
--- app/src/main/java/com/iameben/keysmith/ui/theme/Theme.kt
+++ app/src/main/java/com/iameben/keysmith/ui/theme/Theme.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.text.TextStyle
 
 private val DarkColorScheme = darkColorScheme(
@@ -52,7 +53,7 @@ fun KeySmithTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable (() -> Unit)
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
