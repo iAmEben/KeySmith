85f7a7dcbdd9c28b9d6b470cfd3580ce47f16900
iAmEben <justiceebenezer4@gmail.com>
2025-06-21 22:02:38 +0100

    Updated SwitchViewmodel with enum class

--- app/src/main/java/com/iameben/keysmith/ui/screen/main/SwitchViewmodel.kt
+++ app/src/main/java/com/iameben/keysmith/ui/screen/main/SwitchViewmodel.kt
@@ -3,20 +3,23 @@ package com.iameben.keysmith.ui.screen.main
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import androidx.lifecycle.ViewModel
+import com.iameben.keysmith.ui.components.enums.SwitchType
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class SwitchViewmodel @Inject constructor() : ViewModel() {
-    private val _switchState = MutableStateFlow(false)
-    val switchState: StateFlow<Boolean> = _switchState.asStateFlow()
+    private val _switchStates = MutableStateFlow(mapOf(
+        SwitchType.RANDOM to false,
+        SwitchType.SMART to false,
+        SwitchType.UPPERCASE to false,
+        SwitchType.LOWERCASE to false,
+        SwitchType.NUMBERS to false,
+        SwitchType.SPECIAL_CHARS to false
+    ))
+    val switchStates = _switchStates.asStateFlow()
 
-    fun toggleSwitch() {
-        _switchState.value = !_switchState.value
-    }
-
-    fun setSwitchState(checked: Boolean) {
-        _switchState.value = checked
+    fun toggleSwitch(type: SwitchType, isChecked: Boolean) {
+        _switchStates.value = _switchStates.value.toMutableMap().apply { this[type] = isChecked }
     }
 }
