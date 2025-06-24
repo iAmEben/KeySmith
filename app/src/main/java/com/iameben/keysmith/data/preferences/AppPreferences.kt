f0e50ab9767280b5a6454d883d9928f5c833677f
iAmEben <justiceebenezer4@gmail.com>
2025-06-24 13:22:42 +0100

    Implemented preference for mode selector

--- app/src/main/java/com/iameben/keysmith/data/preferences/AppPreferences.kt
+++ app/src/main/java/com/iameben/keysmith/data/preferences/AppPreferences.kt
@@ -7,6 +7,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 import androidx.core.content.edit
+import com.iameben.keysmith.ui.components.enums.ModeSelector
 
 @Singleton
 class AppPreferences @Inject constructor(@ApplicationContext context: Context) {
@@ -14,10 +15,13 @@ class AppPreferences @Inject constructor(@ApplicationContext context: Context) {
     companion object {
         private const val PREFS_NAME = "app_prefs"
         private const val KEY_SLIDER_VALUE = "slider_value"
+        private const val KEY_MODE_SELECTOR = "mode_selector"
+        private const val DEFAULT_MODE_SELECTOR = "RANDOM"
         private const val DEFAULT_SLIDER_VALUE = 9
     }
 
     private val prefs: SharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        ?: throw IllegalStateException("Context is null")
 
     fun setSwitchState(type: SwitchType, isChecked: Boolean) {
         prefs.edit() { putBoolean(type.name, isChecked) }
@@ -33,6 +37,19 @@ class AppPreferences @Inject constructor(@ApplicationContext context: Context) {
     }
 
     fun getSliderValue(): Int = prefs.getInt(KEY_SLIDER_VALUE, DEFAULT_SLIDER_VALUE)
+
+    fun setMode(mode: ModeSelector) {
+        prefs.edit() { putString(KEY_MODE_SELECTOR, mode.name) }
+    }
+
+    fun getMode(): ModeSelector {
+        val modeName = prefs.getString(KEY_MODE_SELECTOR, DEFAULT_MODE_SELECTOR)
+        return try {
+            ModeSelector.valueOf(modeName ?: DEFAULT_MODE_SELECTOR)
+        } catch (e: IllegalArgumentException) {
+            ModeSelector.RANDOM
+        }
+    }
 
     fun clear() {
         prefs.edit() { clear() }
