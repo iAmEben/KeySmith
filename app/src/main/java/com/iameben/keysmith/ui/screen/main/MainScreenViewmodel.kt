00f2bd3dc4acfb70f81203c1672e0b19adea89f9
iAmEben <justiceebenezer4@gmail.com>
2025-06-26 11:12:33 +0100

    Function for modes and password logics

--- app/src/main/java/com/iameben/keysmith/ui/screen/main/MainScreenViewmodel.kt
+++ app/src/main/java/com/iameben/keysmith/ui/screen/main/MainScreenViewmodel.kt
@@ -6,11 +6,13 @@ import androidx.lifecycle.ViewModel
 import com.iameben.keysmith.data.preferences.AppPreferences
 import com.iameben.keysmith.ui.components.enums.ModeSelector
 import com.iameben.keysmith.ui.components.enums.SwitchType
+import com.iameben.keysmith.util.PasswordGenerator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MainScreenViewmodel @Inject constructor(
+    private val passwordGenerator: PasswordGenerator,
     private val preferences: AppPreferences
 ) : ViewModel() {
     private val _switchStates = MutableStateFlow(preferences.getAllSwitchStates())
@@ -22,9 +24,23 @@ class MainScreenViewmodel @Inject constructor(
     private val _selectMode = MutableStateFlow(preferences.getMode())
     val selectMode = _selectMode.asStateFlow()
 
+    private val _generatedPassword = MutableStateFlow("")
+    val generatedPassword = _generatedPassword.asStateFlow()
+
+    private val _passwordStrength = MutableStateFlow("weak")
+    val passwordStrength = _passwordStrength.asStateFlow()
+
+    init {
+        generatedInitialPassword()
+    }
     fun toggleSwitch(type: SwitchType, isChecked: Boolean) {
-        _switchStates.value = _switchStates.value.toMutableMap().apply { this[type] = isChecked }
+        val currentSwitches = _switchStates.value.toMutableMap()
+        val numSwitchesOn = currentSwitches.values.count { it }
+        if (isChecked && numSwitchesOn >= 3 && _sliderValue.value.toInt() == 4) return
+        currentSwitches[type] = isChecked
+        _switchStates.value = currentSwitches
         preferences.setSwitchState(type, isChecked)
+        generatedPassword()
     }
 
     fun isSwitchOn(type: SwitchType): Boolean = switchStates.value[type] == true
@@ -32,11 +48,51 @@ class MainScreenViewmodel @Inject constructor(
     fun setSliderValue(value: Int) {
         _sliderValue.value = value
         preferences.setSliderValue(value)
+        generatedPassword()
     }
 
     fun setSelectMode(mode: ModeSelector) {
         _selectMode.value = mode
         preferences.setMode(mode)
+        generatedPassword()
+    }
+
+    private fun generatedInitialPassword() {
+        val result = passwordGenerator.generatePassword(
+            mode = _selectMode.value,
+            length = _sliderValue.value.toInt(),
+            switches = _switchStates.value
+        )
+
+        when (result) {
+            is PasswordGenerator.PasswordResult.Success -> {
+                _generatedPassword.value = result.password
+                _passwordStrength.value = result.strength
+            }
+            is PasswordGenerator.PasswordResult.Error -> {
+                _generatedPassword.value = ""
+                _passwordStrength.value = "weak"
+            }
+        }
+    }
+
+    private fun generatedPassword() {
+        val result = passwordGenerator.generatePassword(
+            mode = _selectMode.value,
+            length = _sliderValue.value.toInt(),
+            switches = _switchStates.value
+        )
+
+        when (result) {
+            is PasswordGenerator.PasswordResult.Success -> {
+                _generatedPassword.value = result.password
+                _passwordStrength.value = result.strength
+            }
+            is PasswordGenerator.PasswordResult.Error -> {
+                _generatedPassword.value = ""
+                _passwordStrength.value = "weak"
+            }
+        }
     }
 
 
