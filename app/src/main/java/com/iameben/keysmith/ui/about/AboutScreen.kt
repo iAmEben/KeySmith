225ebdd3d47bba413e827442c722c7b9cfcaca2a
iAmEben <justiceebenezer4@gmail.com>
2025-07-02 21:27:20 +0100

    feat(ui): add screens init

---
+++ app/src/main/java/com/iameben/keysmith/ui/about/AboutScreen.kt
@@ -0,0 +1,8 @@
+package com.iameben.keysmith.ui.about
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AboutScreen() {
+
+}
---
+++ app/src/main/java/com/iameben/keysmith/ui/passwords/PasswordScreen.kt
@@ -0,0 +1,8 @@
+package com.iameben.keysmith.ui.passwords
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PasswordScreen() {
+
+}
