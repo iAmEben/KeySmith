f937b21b562bf31f6874a2a2609bcea57af170dd
iAmEben <justiceebenezer4@gmail.com>
2025-07-02 10:45:49 +0100

    Created screens for settings screen

---
+++ app/src/main/java/com/iameben/keysmith/ui/screen/settings/SettingsViewmodel.kt
@@ -0,0 +1,11 @@
+package com.iameben.keysmith.ui.screen.settings
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewmodel @Inject constructor(
+
+): ViewModel() {
+}
