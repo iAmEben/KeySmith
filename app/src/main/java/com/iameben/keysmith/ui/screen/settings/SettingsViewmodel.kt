3be77e74d387e0017bdd22dfd11cc63330161e23
iAmEben <justiceebenezer4@gmail.com>
2025-07-02 21:25:57 +0100

    feat(ui): add SettingsViewmodel with state flow

--- app/src/main/java/com/iameben/keysmith/ui/screen/settings/SettingsViewmodel.kt
+++ app/src/main/java/com/iameben/keysmith/ui/screen/settings/SettingsViewmodel.kt
@@ -1,11 +1,21 @@
 package com.iameben.keysmith.ui.screen.settings
 
 import androidx.lifecycle.ViewModel
+import com.iameben.keysmith.data.model.SettingsItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingsViewmodel @Inject constructor(
+class SettingsViewmodel @Inject constructor(): ViewModel() {
+    private val _items = MutableStateFlow<List<SettingsItem>>(
+        listOf(
+            SettingsItem("Your Passwords"),
+            SettingsItem("About")
+        )
+    )
 
-): ViewModel() {
+    val items: StateFlow<List<SettingsItem>> = _items
+
 }
