f79ee5bb1ea47297e69ab1f0927704ca551e891e
iAmEben <justiceebenezer4@gmail.com>
2025-07-04 21:22:32 +0100

    feat(database): add function for app database

--- app/src/main/java/com/iameben/keysmith/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/iameben/keysmith/data/local/database/AppDatabase.kt
@@ -1,10 +1,11 @@
 package com.iameben.keysmith.data.local.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.iameben.keysmith.data.local.dao.PasswordDao
 import com.iameben.keysmith.data.local.entity.PasswordEntity
 
 @Database(entities = [PasswordEntity::class], version = 1, exportSchema = false)
-abstract class AppDatabase {
+abstract class AppDatabase: RoomDatabase() {
     abstract fun passwordDao(): PasswordDao
 }
