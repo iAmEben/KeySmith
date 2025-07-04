a60689aa90a01deb3eabbac677778ca15b39a0fd
iAmEben <justiceebenezer4@gmail.com>
2025-07-04 21:21:40 +0100

    feat(database): add function for database and dao in app module

--- app/src/main/java/com/iameben/keysmith/di/AppModule.kt
+++ app/src/main/java/com/iameben/keysmith/di/AppModule.kt
@@ -1,6 +1,9 @@
 package com.iameben.keysmith.di
 
 import android.content.Context
+import androidx.room.Room
+import com.iameben.keysmith.data.local.dao.PasswordDao
+import com.iameben.keysmith.data.local.database.AppDatabase
 import com.iameben.keysmith.data.preferences.AppPreferences
 import com.iameben.keysmith.util.PasswordGenerator
 import com.iameben.keysmith.util.WordListUtil
@@ -26,4 +29,20 @@ object AppModule {
     @Singleton
     fun providePasswordGenerator(preferences: AppPreferences, wordListUtil: WordListUtil): PasswordGenerator =
         PasswordGenerator(preferences, wordListUtil)
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "app_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePasswordDao(database: AppDatabase): PasswordDao {
+        return database.passwordDao()
+    }
 }
