dbb894cd226f2f3c13271319cbd8100d895f8461
iAmEben <justiceebenezer4@gmail.com>
2025-07-04 10:37:08 +0100

    feat(ui): adds back button in Settings Screen

--- app/src/main/java/com/iameben/keysmith/ui/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/iameben/keysmith/ui/screen/settings/SettingsScreen.kt
@@ -5,20 +5,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import com.iameben.keysmith.R
 import com.iameben.keysmith.navigation.Routes
 import com.iameben.keysmith.ui.components.SettingItemRow
 
@@ -41,6 +49,22 @@ fun SettingsScreen(
                 .padding(16.dp)
                 .padding(paddingValues)
         ) {
+
+            IconButton(
+                onClick = { },
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(top = 18.dp)
+                    .clip(CircleShape)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_back),
+                    contentDescription = "back",
+                    tint = MaterialTheme.colorScheme.onBackground,
+                    modifier = Modifier
+                        .size(18.dp)
+                )
+            }
 
             LazyColumn(
                 modifier = Modifier
