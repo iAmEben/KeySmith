4a2232f90f507af5938d5f809e208ad437500791
iAmEben <justiceebenezer4@gmail.com>
2025-07-04 16:11:56 +0100

    feat(DB): adds entity class for Room

---
+++ app/src/main/java/com/iameben/keysmith/data/local/entity/PasswordEntity.kt
@@ -0,0 +1,11 @@
+package com.iameben.keysmith.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "passwords")
+data class PasswordEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val password: String
+)
