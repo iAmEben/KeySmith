bbecd615f1c742c4274e29996d575e03c4690e50
iAmEben <justiceebenezer4@gmail.com>
2025-07-05 14:22:34 +0100

    feat(ui): add password screen

--- app/src/main/java/com/iameben/keysmith/ui/passwords/PasswordScreen.kt
+++ app/src/main/java/com/iameben/keysmith/ui/passwords/PasswordScreen.kt
@@ -1,11 +1,121 @@
 package com.iameben.keysmith.ui.passwords
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.iameben.keysmith.R
+import com.iameben.keysmith.navigation.Routes
+import com.iameben.keysmith.ui.components.SettingItemRow
+import com.iameben.keysmith.util.Space
+import com.iameben.keysmith.util.goBack
 
 @Composable
 fun PasswordScreen(
-    navController: NavHostController
+    navController: NavHostController,
+    viewmodel: PasswordViewmodel = hiltViewModel()
 ) {
+
+    val passwords = viewmodel.passwords.collectAsState().value
+
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(paddingValues)
+        ) {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 18.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                IconButton(
+                    onClick = { goBack(navController) },
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clip(CircleShape)
+
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_back),
+                        contentDescription = "back",
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .size(18.dp)
+                    )
+                }
+
+                Box(
+                    modifier = Modifier
+                        .weight(1f),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Your Passwords",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+
+
+            }
+
+
+            Space(size = 32.dp)
+
+            LazyColumn(
+                modifier = Modifier
+                    .padding(top = 32.dp)
+                    .wrapContentSize()
+                    .background(MaterialTheme.colorScheme.background),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                if (passwords.isEmpty()) {
+                    item {
+                        Text(
+                            text = "No passwords Saved",
+                            style = MaterialTheme.typography.bodyLarge,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
+                } else {
+
+                }
+
+            }
+        }
+    }
 
 }
