fcec72085b92eebb04c69c8b37e1e1d0e2fed500
iAmEben <justiceebenezer4@gmail.com>
2025-07-14 12:06:28 +0100

    feat(ui): implements functions for swipeable

--- app/src/main/java/com/iameben/keysmith/ui/passwords/PasswordScreen.kt
+++ app/src/main/java/com/iameben/keysmith/ui/passwords/PasswordScreen.kt
@@ -18,12 +18,16 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SwipeToDismissBoxState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +37,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.iameben.keysmith.R
+import com.iameben.keysmith.data.local.entity.PasswordEntity
 import com.iameben.keysmith.navigation.Routes
 import com.iameben.keysmith.ui.components.CustomSnackBarHost
 import com.iameben.keysmith.ui.components.PasswordItemRow
@@ -40,6 +45,7 @@ import com.iameben.keysmith.ui.components.SettingItemRow
 import com.iameben.keysmith.ui.screen.main.MainScreenViewmodel
 import com.iameben.keysmith.util.Space
 import com.iameben.keysmith.util.goBack
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
@@ -50,6 +56,8 @@ fun PasswordScreen(
 ) {
 
     val passwords = viewmodel.passwords.collectAsState().value
+    var passwordToDelete by remember { mutableStateOf<PasswordEntity?>(null)}
+    var dismissStateToReset by remember { mutableStateOf<SwipeToDismissBoxState?>(null) }
     val snackBarHostState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
     val context = LocalContext.current
@@ -137,17 +145,21 @@ fun PasswordScreen(
                             onUpdateTitle = { newTitle ->
                                 viewmodel.updatePasswordTitle(password, newTitle)
                             },
-                            onDelete = {
-                                scope.launch {
-                                    viewmodel.deletePassword(password)
-                                }
-
+                            onSwipeToDelete = {
+                                passwordToDelete = password
+                                dismissStateToReset = null
                             },
                             onClick = {
                                 mainScreenViewmodel.copyToClipboard(
                                     context,
                                     text = password.password
                                 )
+                            },
+                            onIconClick = { state ->
+                                passwordToDelete = password
+                                dismissStateToReset = state
+                                scope.launch { state.reset() }
+
                             }
                         )
                     }
