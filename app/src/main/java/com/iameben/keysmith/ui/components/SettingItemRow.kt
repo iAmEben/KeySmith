415b0e3bd7b339aa746c3e7be5a7a2e605737445
iAmEben <justiceebenezer4@gmail.com>
2025-07-16 12:35:26 +0100

    feat(ui): adds card layout to settings lazy columns

--- app/src/main/java/com/iameben/keysmith/ui/components/SettingItemRow.kt
+++ app/src/main/java/com/iameben/keysmith/ui/components/SettingItemRow.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,25 +19,33 @@ import com.iameben.keysmith.util.Space
 
 @Composable
 fun SettingItemRow(item: SettingsItem, onItemClick: () -> Unit) {
-    Box(
+
+    Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onItemClick() }
-            .background(
-                color = MaterialTheme.colorScheme.onPrimary,
-                shape = RoundedCornerShape(12.dp),
+            .wrapContentHeight(),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onPrimary
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onItemClick() }
+                .padding(16.dp)
+        ){
+            Text(
+                text = item.title,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier.padding(8.dp)
 
             )
-            .padding(16.dp)
-    ){
-        Text(
-            text = item.title,
-            style = MaterialTheme.typography.bodyLarge,
-            color = MaterialTheme.colorScheme.onBackground,
-            modifier = Modifier.padding(8.dp)
-
-        )
+        }
     }
+
 
     Space()
 
