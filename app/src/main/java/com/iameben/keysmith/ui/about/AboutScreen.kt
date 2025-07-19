eacb98090fa54cb4908c4047b42d42279d631279
iAmEben <justiceebenezer4@gmail.com>
2025-07-19 14:57:56 +0100

    Adds manrope font to about screen

--- app/src/main/java/com/iameben/keysmith/ui/about/AboutScreen.kt
+++ app/src/main/java/com/iameben/keysmith/ui/about/AboutScreen.kt
@@ -1,11 +1,99 @@
 package com.iameben.keysmith.ui.about
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.iameben.keysmith.R
+import com.iameben.keysmith.util.Space
+import com.iameben.keysmith.util.goBack
 
 @Composable
 fun AboutScreen(
     navController: NavHostController
 ) {
 
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(paddingValues)
+        ) {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                IconButton(
+                    onClick = { goBack(navController) },
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .clip(CircleShape)
+
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_back),
+                        contentDescription = "back",
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .size(18.dp)
+                    )
+                }
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "About",
+                        style = MaterialTheme.typography.headlineSmall,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+
+
+            }
+
+            Text(
+                modifier = Modifier.weight(1f),
+                text = stringResource(id = R.string.about_description),
+                fontFamily = FontFamily(Font(R.font.manrope_variable_font)),
+                lineHeight = 22.sp,
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+
+
+
+        }
+    }
 }
