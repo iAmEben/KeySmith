15a80d63f12f77c70a7547aa1a2696dc4ffdd20a
iAmEben <justiceebenezer4@gmail.com>
2025-07-19 15:21:08 +0100

    feat(ui): adds manrope font family to Type file

--- app/src/main/java/com/iameben/keysmith/ui/theme/Type.kt
+++ app/src/main/java/com/iameben/keysmith/ui/theme/Type.kt
@@ -4,9 +4,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.iameben.keysmith.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -33,4 +35,13 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val Manrope = FontFamily(
+    Font(R.font.manrope_bold, FontWeight.Bold),
+    Font(R.font.manrope_extrabold, FontWeight.ExtraBold),
+    Font(R.font.manrope_extralight, FontWeight.ExtraLight),
+    Font(R.font.manrope_light, FontWeight.Light),
+    Font(R.font.manrope_medium, FontWeight.Medium),
+    Font(R.font.manrope_semibold, FontWeight.SemiBold)
 )
